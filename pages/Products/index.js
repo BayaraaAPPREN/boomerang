2eb1991cf6e9e30c74e96aefedbc7dde3a139017
Doljko <bayarsuren0310@gmail.com>
2022-06-26 22:26:11 +0800

    ahahahah

--- pages/Products/index.js
+++ pages/Products/index.js
@@ -65,7 +65,7 @@ export default function Products(){
                 <div className="grid col-span-1">
                     <div className="">
                         <h1 className="text-5xl text-blue-900 xs:mx-8 mb-8">Hydrating Serum</h1>
-                        <p className="md:w-96 xs:mx-8">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
+                        <p className="md:w-96 xs:mx-8">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry s
                              standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It 
                              has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 
                              1960s with the release of Letraset sheets
