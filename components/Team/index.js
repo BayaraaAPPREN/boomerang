e31eb9645514364e100d4373fd4ccdcefbe01433
Doljko <bayarsuren0310@gmail.com>
2022-07-02 20:11:40 +0800

    ajajja

--- components/Team/index.js
+++ components/Team/index.js
@@ -14,12 +14,12 @@ export default function Team(){
                     <h1 className="text-green-600 border-b-2 py-2 border-w-8">Founder, CEO</h1>
                 </div>
                 <div className="text-center mx-5">
-                    <img src="/tseej2.png"/>
+                    <img src="/tseej021.png"/>
                     <h1 className="text-xl mt-4">NARANGARAV. B </h1>
                     <h1 className="text-green-600 border-b-2 py-2 border-w-8">Head of marketing division</h1>
                 </div>
                 <div className="text-center mx-5">
-                    <img src="/tseej3.png"/>
+                    <img src="/tseej013.png"/>
                     <h1 className="text-xl mt-4">BAYARZAYA. B </h1>
                     <h1 className="text-green-600 border-b-2 py-2 border-w-8 w-60 ml-4">Head of communications and development division</h1>
                 </div>
